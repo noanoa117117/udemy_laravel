206e51003aa3d555aaa2c1a1306c991699bcfe3a
noanoa117117 <89587104+noanoa117117@users.noreply.github.com>
2022-08-19 19:36:47 +0900

    component準備

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -11,7 +11,9 @@
         <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap">
 
         <!-- Scripts -->
-        @vite(['resources/css/app.css', 'resources/js/app.js'])
+        {{-- @vite(['resources/css/app.css', 'resources/js/app.js']) --}}
+        <link rel ="stylesheet" href="{{mix('css/app.css')}}">
+        <scripts src="{{mix('js/app.js')}}" defer></scripts>
     </head>
     <body class="font-sans antialiased">
         <div class="min-h-screen bg-gray-100">
--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -11,8 +11,10 @@
         <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap">
 
         <!-- Scripts -->
-        @vite(['resources/css/app.css', 'resources/js/app.js'])
-    </head>
+        {{-- @vite(['resources/css/app.css', 'resources/js/app.js']) --}}
+        <link rel ="stylesheet" href="{{mix('css/app.css')}}">
+        
+        <scripts src="{{mix('js/app.js')}}" defer></scripts>
     <body>
         <div class="font-sans text-gray-900 antialiased">
             {{ $slot }}
