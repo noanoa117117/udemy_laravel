48ccd231cd2f2d7e17ad196f33ecc618d80ec803
noanoa117117 <89587104+noanoa117117@users.noreply.github.com>
2022-09-25 21:35:02 +0900

    classBase

--- resources/views/tests/component-test2.blade.php
+++ resources/views/tests/component-test2.blade.php
@@ -1,4 +1,8 @@
 <x-tests.app>
     <x-slot name="header">ヘッダー2</x-slot>
-コンポーネントテスト2
+    コンポーネントテスト2
+
+    <x-test-class-base classBaseMessage='message' />
+    <div class="mb-4"></div>
+    <x-test-class-base classBaseMessage='message' defaultMessage='初期値から変更' />
 </x-tests.app>
