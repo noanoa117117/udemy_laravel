d3ac7f50942c1ac186316089031a75d2bcf7ce3f
noanoa117117 <sinigamiyuuna@gmail.com>
2022-11-23 04:04:45 +0900

    middleware,abort(404)

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use App\Providers\session;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -23,6 +24,11 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        if(request()->is('owner*')){
+            config(['session.cookie' => config('session.cookie_owner')]);
+        }
+        if(request()->is('admin*')){
+            config(['session.cookie' => config('session.cookie_admin')]);
+        }
     }
 }
